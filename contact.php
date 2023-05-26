e33b3f604e860e006dc6467d08c28ccb1590106f
Alex Liu <alex369888@gmail.com>
2023-05-26 02:05:51 -0700

    Contact Form Test

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Contact | Alex Liu</title>
+</head>
+<body>
+    <p>SEND E-MAIL</p>
+    <form action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Full Name">
+        <input type="text" name="mail" placeholder="E-mail">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
